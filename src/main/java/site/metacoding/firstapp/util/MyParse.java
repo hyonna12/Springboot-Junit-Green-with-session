07563d083cc0b34f44cfb3aa1f31c0bb875716c5
hyonna12 <hyonna01@gmail.com>
2022-09-29 16:47:36 +0900

    Product DAO 완료

---
+++ src/main/java/site/metacoding/firstapp/util/MyParse.java
@@ -0,0 +1,9 @@
+package site.metacoding.firstapp.util;
+
+public class MyParse {
+  // 문자로 들어온걸 숫자로 바꾸는 메서드
+  public int changeStringToint(String value) {
+    return Integer.parseInt(value);
+  }
+
+}
